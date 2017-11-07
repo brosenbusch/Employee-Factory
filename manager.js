30c309a75370fb95c1927fb53f14f22589575118
brosenbusch <8brosenbusch@stpatrick.org>
2017-11-07 14:42:02 -0600

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,8 +1,23 @@
-const thing = require('./thing.js');
+const random = require('./Random.js');
 
-const moduleName = function(){
+const manager = function(){
+    let id = "m-" + random().digit(8); 
+    let workers = random().discreteRangeIn(4,20); 
+    let baseSalary = random().discreteRangeIn(4,9)*10000;
 
-
+    function pay(){
+        if(workers <=10){
+            workers*250
+        }
+        else if(workers > 10){
+            workers 
+        }
+        let dailyPay = (hours/5)*wage;
+        let weeklyPay = hours*wage;
+        let yearlyPay = weeklyPay*52;
+        
+        return [dailyPay, weeklyPay, yearlyPay];  
+    }
 };
 
-module.exports = moduleName;
+module.exports = worker;
