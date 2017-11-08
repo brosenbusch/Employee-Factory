53f3bde9336ebc9412b742f58ca53ee55fb38f0d
brosenbusch <8brosenbusch@stpatrick.org>
2017-11-08 14:29:36 -0600

    Update divisionHead.js

--- divisionHead.js
+++ divisionHead.js
@@ -1,8 +1,21 @@
-const thing = require('./thing.js');
+const random = require('./Random.js');
 
-const moduleName = function(){
+const divisionHead = function(){
+    let id = "d-" + random().digit(8); 
+    let managers = random().discreteRangeIn(1,10);
+    let workers = random().discreteRangeIn(20,300); 
+    let baseSalary = random().discreteRangeIn(2,5)*50000;
 
-
+    function pay(){
+        let yearlyPay = baseSalary;
+        yearlyPay += managers*10000;
+        yearlyPay += workers*50;
+        let weeklyPay = yearlyPay/52;
+        let dailyPay = weeklyPay/7;
+        
+        
+        return [yearlyPay, weeklyPay, dailyPay];  
+    }
 };
 
-module.exports = moduleName;
+module.exports = divisionHead;
